4c48c032e92beedf9bc9a7c9d36a17d9c73d7171
nchoo.wsl <nicchooyf@gmail.com>
2023-03-23 18:48:27 +0800

    minor tidying up. prepare for merge

--- srcs/ms_shell_loop.c
+++ srcs/ms_shell_loop.c
@@ -6,7 +6,7 @@
 /*   By: nchoo <nchoo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 14:42:57 by plau              #+#    #+#             */
-/*   Updated: 2023/03/23 12:47:29 by nchoo            ###   ########.fr       */
+/*   Updated: 2023/03/23 18:47:42 by nchoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,13 +69,10 @@ int	parsing(t_prg *prg)
 	if (prg->all_token == NULL)
 		return (1) ;
 	prg->all_token = expand_tokens(prg);
-	// print_tokens(prg);
-	//print_tokens(prg);
 	if (prg->exp->quote != 1)
 		prg->all_token = expand_tokens(prg);
 	prg->all_token = remove_quotes(prg);
 	print_tokens(prg);
-	// prg->all_token = ft_split(prg->input, ' ');
 	count_pipe_n_heredoc(prg);
 	return (0);
 }
