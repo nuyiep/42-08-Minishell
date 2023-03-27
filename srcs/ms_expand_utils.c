94f675f064adcf02167c371105e964f11b01b752
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-03-27 14:35:07 +0800

    commented print var

--- srcs/ms_expand_utils.c
+++ srcs/ms_expand_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_expand_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchoo <nchoo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 15:29:40 by nchoo             #+#    #+#             */
-/*   Updated: 2023/03/25 14:45:08 by nchoo            ###   ########.fr       */
+/*   Updated: 2023/03/27 14:34:50 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ char *get_var(char *token, int i)
 	&& token[i] != '\'' && token[i] != '\"')
 		var[j++] = token[i++];
 	var[j] = '\0';
-	ft_printf("var: %s\n", var);
+	//ft_printf("var: %s\n", var);
 	return (var);
 }
